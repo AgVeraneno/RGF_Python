86b24df3080133dd2571caf3135150681b27e587
alexlin <alexlin@artiluxtech.com>
2018-11-30 17:07:53 +0800

    revise band structure output

--- src/band_structure.py
+++ src/band_structure.py
@@ -5,23 +5,15 @@ from matplotlib import pyplot
 class BandStructure():
     def __init__(self, inputs):
         self.inputs = inputs        # user input
-        if inputs['GPU']['enable']:
-            global cp
-            import cupy as cp
-    def calState(self, unit):
+        self.mesh = inputs['mesh']
+    def calState(self, unit, idx):
+        unit.setKx(idx)
         mat = self.inputs['material']
         H = unit.H+\
             np.exp(1j*unit.kx*mat.ax)*unit.P_plus+\
             np.exp(-1j*unit.kx*mat.ax)*unit.P_minus
         val, vec = np.linalg.eig(H)
         return self.__sort__(val, vec)
-    def calState_GPU(self, unit):
-        mat = self.inputs['material']
-        H = cp.asarray(unit.H)+\
-            cp.exp(1j*unit.kx*mat.ax)*cp.asarray(unit.P_plus)+\
-            cp.exp(-1j*unit.kx*mat.ax)*cp.asarray(unit.P_minus)
-        val, vec = cp.linalg.eigh(H)
-        return self.__sort__(val, vec, 'GPU')
     def plotBand(self, bandgap_list, unit_idx):
         ## construct vectors
         eig_mat = np.real(np.array(bandgap_list['y']))
@@ -67,18 +59,19 @@ class BandStructure():
                 os.mkdir('figures')
                 pyplot.savefig('figures/'+filename+condition+'_zoom.png')
             pyplot.close()
-    def __sort__(self, val, vec, caltype='CPU'):
+    def __sort__(self, val, vec):
         """
         What: Sort eigenstate with small to large sequence
         How: 1.Sweep original eigenvalue and match sorted one.
-             2.Copy the original eigenstate to a new array. 
+             2.Copy the original eigenstate to a new array.
+        inputs:
+        val: eigenvalue [n*n]
+        vec: eigenstate [n*n]
         """
-        if caltype == 'GPU':
-            output_vec = cp.zeros((cp.size(vec,0),cp.size(vec,0)), dtype=cp.complex128)
-            sorted_val = cp.sort(val)
-        else:
-            output_vec = np.zeros((np.size(vec,0),np.size(vec,0)), dtype=np.complex128)
-            sorted_val = np.sort(val)
+        vec_size = np.size(vec,0)
+        output_vec = np.zeros((vec_size,vec_size), dtype=np.complex128)
+        ## first kx point ##
+        sorted_val = np.sort(val)
         for v1_idx, v1 in enumerate(val):
             for v2_idx, v2 in enumerate(sorted_val):
                 if v1 == v2:
